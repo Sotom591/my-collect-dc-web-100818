7138bf5390b288bf8981e213bcaaec310f1ee40b
Sotom591 <sotom591@gmail.com>
2018-09-29 18:43:39 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
 
 
+def my_collect(diffarrays)
+empty_array = []
+counter = 0
+while counter < diffarrays.length
+empty_array << yield(diffarrays[counter])
+counter += 1
+end
+empty_array
+end 
+#my_collect(languages) do |language| language.uppercase
+#end
+#my_collect(students) do |name| name.split(" ").first
+#end
+
+
+
